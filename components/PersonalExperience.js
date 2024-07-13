3c96badea043f8318327b834d92daf92d779327f
AaronHsieh <piafyoyo06@gmail.com>
2024-07-13 19:43:41 +0800

    新增個人簡歷內文

--- components/PersonalExperience.js
+++ components/PersonalExperience.js
@@ -7,7 +7,33 @@ export default function PersonalExperience() {
   return (
     <section className={styles.container}>
       <div className={styles.innerWrapper}>
-        <HeadingWithLine Heading="Vito Evans" />
+        <article className={styles.experienceArticle}>
+          <HeadingWithLine Heading="Vito Evans" />
+          <p>456 Walnut Avenue, Someville, USA</p>
+          <p>+1 123 456 7890</p>
+          <p>info@hexschool.com</p>
+          <p>www.infohexschool.com</p>
+        </article>
+        <article className={styles.experienceArticle}>
+          <HeadingWithLine Heading="Education" />
+          <p>456 Walnut Avenue, Someville, USA</p>
+          <p>+1 123 456 7890</p>
+          <p>info@hexschool.com</p>
+          <p>www.infohexschool.com</p>
+        </article>
+        <article className={styles.experienceArticle}>
+          <HeadingWithLine Heading="Work" />
+          <p>456 Walnut Avenue, Someville, USA</p>
+          <p>+1 123 456 7890</p>
+          <p>info@hexschool.com</p>
+          <p>www.infohexschool.com</p>
+        </article>
+        <article className={styles.experienceArticle}>
+          <HeadingWithLine Heading="Awards" />
+          <p>Best in Show Award, National Advertising Awards, 2020</p>
+          <p>Honorable Mention, International Design Competition, 2019</p>
+          <p>People's Choice Award, Local Art Exhibition, 2018</p>
+        </article>
       </div>
     </section>
   );
