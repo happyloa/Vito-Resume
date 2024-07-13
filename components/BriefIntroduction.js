9a56c508da3fa2cbf4170f3f8b3900d7997f9f04
AaronHsieh <piafyoyo06@gmail.com>
2024-07-13 19:02:29 +0800

    做出簡介部分

--- components/BriefIntroduction.js
+++ components/BriefIntroduction.js
@@ -3,7 +3,16 @@ import styles from "./BriefIntroduction.module.css";
 export default function BriefIntroduction() {
   return (
     <>
-      <section>456</section>
+      <section className={styles.container}>
+        <div className={styles.innerWrapper}>
+          <p className={styles.intro}>
+            I'm Vito, a UI Designer with 5+ years of experience. I create
+            visually appealing interfaces for web and mobile apps. Proficient in
+            Sketch, Adobe XD, Figma, HTML, CSS, and JavaScript. I enjoy
+            collaborating with others to create high-quality products.
+          </p>
+        </div>
+      </section>
     </>
   );
 }
