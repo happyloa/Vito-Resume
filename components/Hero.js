dee54e0eb6d4234a838401b20793bc5a21113b97
AaronHsieh <piafyoyo06@gmail.com>
2024-07-13 19:22:23 +0800

    移除 Fragment

--- components/Hero.js
+++ components/Hero.js
@@ -2,18 +2,16 @@ import styles from "./Hero.module.css";
 
 export default function Hero() {
   return (
-    <>
-      <section className={styles.container}>
-        <div className={styles.innerWrapper}>
-          <h1 className={styles.name}>VITO</h1>
-          <img
-            src="image/vito.webp"
-            alt="Vito 個人照"
-            className={styles.avatar}
-          />
-          <h2 className={styles.heading}>RESUME</h2>
-        </div>
-      </section>
-    </>
+    <section className={styles.container}>
+      <div className={styles.innerWrapper}>
+        <h1 className={styles.name}>VITO</h1>
+        <img
+          src="image/vito.webp"
+          alt="Vito 個人照"
+          className={styles.avatar}
+        />
+        <h2 className={styles.heading}>RESUME</h2>
+      </div>
+    </section>
   );
 }
