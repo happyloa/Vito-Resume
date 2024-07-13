93dff5d703afd74a6783ade2b64daf53a63789a3
AaronHsieh <piafyoyo06@gmail.com>
2024-07-13 19:36:40 +0800

    引入並使用帶有下底線的標題元件

--- components/PersonalExperience.js
+++ components/PersonalExperience.js
@@ -1,9 +1,14 @@
 import styles from "./PersonalExperience.module.css";
 
+// 引入帶有裝飾線的標題展示性元件
+import HeadingWithLine from "./ui/HeadingWIthLine";
+
 export default function PersonalExperience() {
   return (
-    <>
-      <section>789</section>
-    </>
+    <section className={styles.container}>
+      <div className={styles.innerWrapper}>
+        <HeadingWithLine Heading="Vito Evans" />
+      </div>
+    </section>
   );
 }
