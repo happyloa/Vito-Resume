9d3484fab3c53c6445e4cc01be386752e5c4c67f
AaronHsieh <piafyoyo06@gmail.com>
2024-09-02 06:36:25 +0800

    新增社群按鈕懸停時的動畫效果

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+import { gsap } from "gsap";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
+  useEffect(() => {
+    gsap.utils.toArray(`.${styles.footer} img`).forEach((img) => {
+      img.addEventListener("mouseenter", () => {
+        gsap.to(img, { y: -10, duration: 0.3, ease: "bounce.out" });
+      });
+      img.addEventListener("mouseleave", () => {
+        gsap.to(img, { y: 0, duration: 0.3, ease: "bounce.out" });
+      });
+    });
+  }, []);
+
   return (
     <footer className={styles.footer}>
       <a href="#" target="_blank">
